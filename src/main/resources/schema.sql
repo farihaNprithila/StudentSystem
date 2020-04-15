05ae9ff3c51b1ff52f526b4d9f21236df8886e34
Fariha Nawaz <fariha.nawaz@g.bracu.ac.bd>
2020-04-15 22:33:34 +0600

    modifying the schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS student (
+CREATE TABLE student (
     student_id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100),
     address VARCHAR(100)
 );
-CREATE TABLE IF NOT EXISTS course (
+CREATE TABLE course (
     course_id INT NOT NULL PRIMARY KEY,
     course_name VARCHAR(50) NOT NULL,
     course_instructor VARCHAR(100)
 );
-CREATE TABLE IF NOT EXISTS department(
+CREATE TABLE department(
     department_id INT NOT NULL PRIMARY KEY,
     department_name VARCHAR(50) NOT NULL
 );
