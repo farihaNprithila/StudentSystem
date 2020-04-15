fe89c31c44ecf0ed4479d23081cfe42deae9b43b
Fariha Nawaz <fariha.nawaz@g.bracu.ac.bd>
2020-04-15 22:56:50 +0600

    modifying the department POJO

--- src/main/java/StudentSystem/Model/Department.java
+++ src/main/java/StudentSystem/Model/Department.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 //
 /**
  * Created by Fariha Nawaz on 13,Apr,2020.
@@ -13,10 +14,14 @@ import javax.persistence.Table;
 public class Department {
 
     @Id
-    @Column(name = "department_id",  nullable = false)
+    @Column(name = "id",  nullable = false)
     private long departmentId;
-    @Column(name = "department_name",  nullable = false)
+
+    @Column(name = "name",  nullable = false)
     private String departmentName;
+
+    public Department() {
+    }
 
     public Department(long departmentId, String departmentName) {
         this.departmentId = departmentId;
@@ -37,5 +42,18 @@ public class Department {
 
     public void setDepartmentName(String departmentName) {
         this.departmentName = departmentName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return departmentId == that.departmentId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(departmentId);
     }
 }
