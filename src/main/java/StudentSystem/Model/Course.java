e0fc151e044d4698ce060f633b7d35acefa6dca7
Fariha Nawaz <fariha.nawaz@g.bracu.ac.bd>
2020-04-15 22:46:39 +0600

    modifying the schema

--- src/main/java/StudentSystem/Model/Course.java
+++ src/main/java/StudentSystem/Model/Course.java
@@ -5,6 +5,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 /**
  * Created by Fariha Nawaz on 13,Apr,2020.
@@ -14,12 +15,17 @@ import javax.persistence.Table;
 public class Course {
 
     @Id
-    @Column(name = "course_id",nullable = false)
+    @Column(name = "course_id",nullable = false,updatable = false)
     private long courseId;
+
     @Column(name = "course_name", nullable = false)
     private String courseName;
+
     @Column(name = "course_instructor", nullable = false)
     private String courseInstructor;
+
+    public Course() {
+    }
 
     public Course(long courseId, String courseName, String courseInstructor) {
         this.courseId = courseId;
@@ -51,4 +57,16 @@ public class Course {
         this.courseInstructor = courseInstructor;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return courseId == course.courseId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(courseId);
+    }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE student (
-    student_id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100),
     address VARCHAR(100)
 );
 CREATE TABLE course (
-    course_id INT NOT NULL PRIMARY KEY,
-    course_name VARCHAR(50) NOT NULL,
-    course_instructor VARCHAR(100)
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    instructor VARCHAR(100)
 );
 CREATE TABLE department(
-    department_id INT NOT NULL PRIMARY KEY,
-    department_name VARCHAR(50) NOT NULL
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
